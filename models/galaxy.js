3b0169730a7e4275cb7112dffa4c85a4bc468dc9
Savannah Victoria <savannahcv@Savannahs-MacBook-Pro.local>
2024-11-18 22:45:23 -0600

    Docker Container restart

--- models/galaxy.js
+++ models/galaxy.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.Galaxy.hasMany(models.Stars)
+      models.Galaxy.hasMany(models.Star)
 
     }
   }
